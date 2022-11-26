182ac7f4cf14b879c215d39f8c86c0966d926a42
pppclove-notebook <pppclove29@naver.com>
2022-11-26 15:45:15 +0900

    타 컴퓨터로 깃 동기화

---
+++ src/main/java/com/example/bookcopy/config/auth/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.bookcopy.config.auth;
+
+import com.example.bookcopy.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity // 시큐리티에 기본으로 필요한 놈
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+    } // 너무 길고 복잡하다 book p.181
+}
